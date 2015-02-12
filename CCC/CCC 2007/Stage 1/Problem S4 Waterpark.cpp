6b54a59a69affc42cd8e1915b5a194357cdd3a37
William G. <will.grand.wg@gmail.com>
2015-02-11 23:29:08 -0500

    Update Problem S4 Waterpark.cpp

--- CCC/CCC 2007/Stage 1/Problem S4 Waterpark.cpp
+++ CCC/CCC 2007/Stage 1/Problem S4 Waterpark.cpp
@@ -1,20 +1,26 @@
-#include<iostream>
-#include<vector>
-#include<string.h>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
 using namespace std;
-vector<int> s[10000];
-int n,a,b,slides;
-int main(){
-    cin >> n;
-    int p[n+1];
-    memset(p,0,sizeof p);
-    while(1){cin >> a >> b; if(a == 0) break; s[a].push_back(b);}
-    p[1] = 1;
-    for(int x = 1;x < n ;x++){
-        for(int y = 0;y < s[x].size();y++){
-            p[s[x][y]] += p[x];
-        }
-    }
-    cout << p[n] << endl;
-}
 
+const int MAXN = 10001;
+int N,a,b;
+int p[MAXN];
+vector<int>adj[MAXN];
+
+int main() {
+	scanf("%d",&N);
+	while(true){
+		scanf("%d%d",&a,&b);
+		if(a==0 && b==0)break;
+		adj[a].push_back(b);
+	}
+	p[1]=1;
+	for(int i = 1; i < N;i++)
+		for(int j = 0; j < adj[i].size();j++)
+			p[adj[i][j]]+=p[i];
+	printf("%d\n",p[N]);
+	return 0;
+}
