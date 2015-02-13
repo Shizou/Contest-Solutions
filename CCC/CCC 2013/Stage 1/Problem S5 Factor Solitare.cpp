8feca6b033def091bbfe8f5ea7594ad5b923ac5b
William G. <will.grand.wg@gmail.com>
2015-02-13 16:52:04 -0500

    Update Problem S5 Factor Solitare.cpp

--- CCC/CCC 2013/Stage 1/Problem S5 Factor Solitare.cpp
+++ CCC/CCC 2013/Stage 1/Problem S5 Factor Solitare.cpp
@@ -1,4 +1,4 @@
-/*DISCLAIMER, NOT ALWAYS CORRECT AND TOO SLOW BUT KINDA COOL*/
+/*TLES ON LAST THREE TESTCASES*/
 #include<iostream>
 #include<cmath>
 #include<cstdio>
@@ -8,50 +8,25 @@
 #define mp make_pair
 using namespace std;
 
+const int MAXN = 5*(int)1e6+5,INF = 0x3F3F3F3F;
 int N;
-vector<int>factors;
-vector<int>cost(5000000,1<<23);
+vector<int>dp(MAXN,INF);
 
-void sieve(){
-    vector<bool>v(N+5,true);
-    v[0]=v[1]=false;
-    factors.push_back(1);
-    for(int i = 2; i < sqrt(N+5);++i){
-        if(v[i]){
-            for(int j = i*i;j < N+5;j+=i)v[j]=false;
-            factors.push_back(i);
-        }
-    }
-    for(int i = sqrt(N)-1;i < N+5;++i)
-        if(v[i])factors.push_back(i);
-}
 int main(){
     //freopen("input.txt","r",stdin);
     scanf("%d",&N);
-    sieve();
-    // minimize cost using a min heap so we know for sure
-    // when we get to N that this will be the minimum cost.
-    priority_queue< pair<int,int> >q;
-    q.push(mp(0,1));
-    while(!q.empty()){
-        int cst = -(q.top().first);
-        int c   = q.top().second;
-        q.pop();
-        if(c > N || cost[c] <= cst)
-            continue;
-        else
-            cost[c] = cst;
-        if(c == N){
-            printf("%d\n",cst);
-            break;
-        }
-        for(int i = 0; i < factors.size() && factors[i] <= c;++i){
-            if(c%factors[i] == 0){
-                int a = factors[i],b = c/factors[i];
-                q.push(mp(-(cst+b),c+a));
-                q.push(mp(-(cst+a),c+b));
+    dp[1] = 0;
+    for(int i = 1; i <= N;i++){
+        for(int j = 1;j <= (int)sqrt(i);j++){
+            if(i%j==0){
+                if(dp[i+j] > dp[i] + i/j)
+                    dp[i+j] = dp[i]+i/j;
+                if(dp[i+i/j] > dp[i]+j)
+                    dp[i+i/j] = dp[i]+j;
             }
+
         }
     }
+    printf("%d\n",dp[N]);
     return 0;
 }
