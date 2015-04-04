4b10b4c2a07b0747b01eb3522ff720696dd36a08
William G <will.grand.wg@gmail.com>
2015-04-04 13:36:40 -0400

    new solution

--- IOI/IOI'14/IOI'14 Practice Task - Station.cpp
+++ IOI/IOI'14/IOI'14 Practice Task - Station.cpp
@@ -6,27 +6,24 @@ using namespace std;
 
 int N,K;
 set<int>stn;
+set<int>::iterator it;
 
 int main(){
     scanf("%d%d",&N,&K);
     for(int i = 1,j = 0;i <= N;++i){
         scanf("%d",&j);
-        if(j == 1)stn.insert(i);
+        if(j != 0)stn.insert(i);
     }
     int pos = 1,cnt = 0;
     while(pos != N){
-        int next = *stn.upper_bound(pos);
-        while(abs(pos-*stn.upper_bound(next)) <= K){
-            next = *stn.upper_bound(next);
-            if(next == N)break;
-        }
-        if(abs(pos-next) <= K){
-          pos = next;
-          cnt++;
+        it = --stn.upper_bound(pos+K);
+        if(*it == pos || *it-pos > K){
+            cnt = -1;
+            break;
         }
         else{
-          cnt = -1;
-          break;
+          pos = *it;
+          cnt++;
         }
     }
     printf("%d\n",cnt);
