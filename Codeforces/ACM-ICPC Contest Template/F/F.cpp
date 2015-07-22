0ccfa6b6904afa5890f7103160c6cbfa76193fe5
William G <will.grand.wg@gmail.com>
2015-07-21 23:25:36 -0400

    change to template

--- Codeforces/ACM-ICPC Contest Template/F/F.cpp
+++ Codeforces/ACM-ICPC Contest Template/F/F.cpp
@@ -1,5 +1,7 @@
 #include<bits/stdc++.h>
-#define needforspeed ios::sync_with_stdio(0);cin.tie(0)
+#define needforspeed ios::sync_with_stdio(0);cin.tie(0);
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
 #define endl '\n'
 #define pb push_back
 #define mp make_pair
@@ -8,7 +10,8 @@
 #define trace1(a) cout << (a) << endl;
 #define trace2(a,b) cout << (a)  << " " << (b) << endl;
 #define trace3(a,b,c) cout << (a)  << " " << (b) << " " << (c) << endl;
-#define trace4(a,b,c) cout << (a)  << " " << (b) << " " << (c) <<  << (d) << endl;
+#define trace4(a,b,c,d) cout << (a)  << " " << (b) << " " << (c) <<  " " << (d) << endl;
+#define trace5(a,b,c,d,e) cout << (a)  << " " << (b) << " " << (c) <<  " " << (d) <<  " " << (e) << endl;
 #define ms(a,b) memset( (a), (b), sizeof(a))
 #define fi(x) freopen(x,"r",stdin)
 #define fo(x) freopen(x,"w",stdout)
@@ -17,16 +20,16 @@
 #define wk(x) for(int  (k) = (0); (k) < (x);(k)++)
 #define all(x) (x).begin(),(x).end()
 #define len(x) (int)(x).size()
-#define x first
-#define y second
+#define xx first
+#define yy second
 #define ll long long
+#define pii pair<int,int>
+#define pll pair<ll,ll>
 #define MAXN (int)1e9
 #define inf 0x3f3f3f3f
 #define nullptr 0
 #define db 0
 using namespace std;
-
-int N;
 
 int main() {
   fi("F-input.txt");
