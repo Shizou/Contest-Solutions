099df34629bbdf30bb4ba301c72d6b58d74792e1
William Granados <william..granadosmatamoros@mail.utoronto.ca>
2016-01-21 15:58:00 -0500

    testing2

--- test.cpp
+++ test.cpp
@@ -1,8 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+const int N = 7;
+
 int main()
 {
   cout << "testing" << endl;
+  for(int i = 0;i < N;i++)
+  {
+      for(int j = 0;j < N;j++)
+      {
+          cout << i+j << endl;
+      }
+  }
   return 0;
 }
