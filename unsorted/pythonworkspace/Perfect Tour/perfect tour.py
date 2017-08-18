9c53a6d330c26da4566a554decf488b8a9a8e121
William Granados <me@wgma00.me>
2017-08-18 01:18:04 -0400

    moved python workspace to cplusplus workspace

--- unsorted/pythonworkspace/Perfect Tour/perfect tour.py
+++
@@ -1,4 +0,0 @@
-N, M = map(int, raw_input().strip().split())
-best = [-101 for i in range(0, 20000)]
-for i in range(M):
-    a = [input().strip().split()]
--- unsorted/pythonworkspace/Task AUTORI/autori.py
+++
@@ -1,3 +0,0 @@
-n = input().split('-')
-for i in range(0, len(n)):
-    print(n[i][0],end='')
