c5c162e4c75af1e0cfcd03f238ea7130e91e8cb6
William Granados <me@wgma00.me>
2017-12-09 06:06:47 -0500

    solved aoc9

---
+++ aoc17/advent09/pt1.rb
@@ -0,0 +1,47 @@
+def calc_score(line)
+	stack = []
+	trash = false
+	sum = 0
+	i = 0
+	# puts line.size
+	while i != line.size
+		# print i
+		if line[i] == "!"
+			i += 1 
+			# print ". ! and we skipping to: " + i.to_s
+		elsif not trash
+			# print ": dis is NOT trash" 
+			if line[i] == "{"
+				if stack.empty?
+					# print ": starting a new"
+					stack << ["{", 1]
+				else
+					# print ": imbedded group"
+					stack <<["{", stack[-1][1]+1]
+					# print ": adding" + stack[-1].to_s
+				end
+			elsif line[i] == "<"
+				trash = true
+			elsif line[i] == "}"	
+				# print ": now we popping" + line[i-1] + " "
+				elem = stack.pop
+				# print elem
+				sum += elem[1]
+			end
+		elsif trash
+			# print ": dis is trash" 
+			if line[i] == ">"
+				trash = false
+			end
+		end
+		# print "\n"
+		i += 1
+	end
+	return sum
+end
+
+line = gets.to_s.tr(" ", "")
+while line != ""
+	puts calc_score(line)
+	line = gets.to_s.tr(" ", "")
+end
